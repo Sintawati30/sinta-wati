3da72e26702714118d82801b6a742fd6aa65ae58
Sinta wati <96229282+Sintawati30@users.noreply.github.com>
2021-12-18 11:21:10 +0700

    Add files via upload

---
+++ lingkaran.cpp
@@ -0,0 +1,18 @@
+#include	<iostream>
+
+using namespace std;
+
+int main(){
+	float lp.lk.lt.r;
+	double jumlah, hasil;
+	const float phi=3.14;
+	cout<<"=========================\n ";
+	cout<<"program menghitung luas lingkaran\n";
+	cout<<"=========================\n";
+	cout<<"luas lingkaran 1 : "<<lk<<endl;
+	
+	
+	
+}
+
+
---
+++ tugas1.cpp
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+	printf("\nNIM			: 07.1.2021.0051 ");
+	printf("\nNAMA			: SINTA WATI ");
+	printf("\nPRODI			: TEKNIK INFORMATIKA");
+	
+	return 0;
+}
+
